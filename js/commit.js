60d3619abb5ea4ecb1ac161c1dfada02d5cbc474
Xwilarg <xwilarg@yahoo.fr>
2018-10-19 11:19:46 +0200

    Increase backend operation to reduce what the user download

--- js/commit.js
+++ js/commit.js
@@ -2,14 +2,7 @@ function analyze(token) {
     result = "";
     brokenRules = {};
     document.getElementById("result").innerHTML = "Loading, please wait...";
-    var http = new XMLHttpRequest();
-    http.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            checkRules(this.responseText);
-        }
-    };
-    http.open("GET", "php/getVerbs.php", true);
-    http.send();
+    getCommit(document.getElementById("author").value, document.getElementById("repo").value, 1, this.responseText);
 }
 
 function increaseBrokenRule(brokenRules, name) {
@@ -25,29 +18,24 @@ function getCommit(author, repo, page, verbs) {
     http.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var json = JSON.parse(this.responseText);
-            var needNext = (json.length < 100) ? false : true;
             json.forEach(function(elem) {
-                var msg = elem.commit.message;
-                var subject = msg.split('\n')[0];
-                var name = elem.commit.author.name;
-                result += "<tr><td>" + name + "</td><td>" + msg + "</td><td>"
-                if (!subject.match("Merge branch[ 'a-zA-Z0-9_-]+ of https:\/\/github.com\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+")) {
-                    if (!checkRule2(subject)) {
-                        result += '<a href="https://chris.beams.io/posts/git-commit/#limit-50">Rule 2</a> ';
-                        increaseBrokenRule(brokenRules, name);
-                    }
-                    if (!checkRule3(subject)) {
-                        result += '<a href="https://chris.beams.io/posts/git-commit/#capitalize">Rule 3</a> ';
-                        increaseBrokenRule(brokenRules, name);
-                    }
-                    if (!checkRule4(subject)) {
-                        result += '<a href="https://chris.beams.io/posts/git-commit/#end">Rule 4</a> ';
-                        increaseBrokenRule(brokenRules, name);
-                    }
-                    if (!checkRule5(subject, verbs)) {
-                        result += '<a href="https://chris.beams.io/posts/git-commit/#imperative">Rule 5</a> ';
-                        increaseBrokenRule(brokenRules, name);
-                    }
+                var name = elem[0];
+                result += "<tr><td>" + name + "</td><td>" + elem[1] + "</td><td>";
+                if (!elem[2][1]) {
+                    result += '<a href="https://chris.beams.io/posts/git-commit/#limit-50">Rule 2</a> ';
+                    increaseBrokenRule(brokenRules, name);
+                }
+                if (!elem[2][2]) {
+                    result += '<a href="https://chris.beams.io/posts/git-commit/#capitalize">Rule 3</a> ';
+                    increaseBrokenRule(brokenRules, name);
+                }
+                if (!elem[2][3]) {
+                    result += '<a href="https://chris.beams.io/posts/git-commit/#end">Rule 4</a> ';
+                    increaseBrokenRule(brokenRules, name);
+                }
+                if (!elem[2][4]) {
+                    result += '<a href="https://chris.beams.io/posts/git-commit/#imperative">Rule 5</a> ';
+                    increaseBrokenRule(brokenRules, name);
                 }
                 result = result.replace(new RegExp('\n', 'g'), '<br/>');
                 result += "</td></tr>";
@@ -57,54 +45,8 @@ function getCommit(author, repo, page, verbs) {
                 brokenResults += key + ": x" + brokenRules[key] + "<br/>";
             }
             document.getElementById("result").innerHTML = "<br/>" + brokenResults + "<br/>" + intro + result + "</table>";
-            if (needNext) {
-                getCommit(author, repo, page + 1, verbs);
-            }
         }
     };
     http.open("GET", "php/request.php?author=" + author + "&repo=" + repo + "&page=" + page, true);
     http.send();
-}
-
-function checkRules(verbs) {
-    document.getElementById("result").innerHTML = "Loading, please wait...";
-    getCommit(document.getElementById("author").value, document.getElementById("repo").value, 1, verbs);
-}
-
-function checkRule1() { // Separate subject from body with a blank line
-    return (true);
-}
-
-function checkRule2(subjectLine) { // Limit the subject line to 50 characters
-    if (subjectLine.length > 50)
-        return (false);
-    return (true);
-}
-
-function checkRule3(subjectLine) { // Capitalize the subject line
-    if (subjectLine[0] < 'A' || subjectLine[0] > 'Z')
-        return (false);
-    return (true);
-}
-
-function checkRule4(subjectLine) { // Do not end the subject line with a period
-    var last = subjectLine.slice(-1);
-    if (last == '.' || last == ',' || last == ';' || last == ':')
-        return (false);
-    return (true);
-}
-
-function checkRule5(subjectLine, verbs) { // Use the imperative mood in the subject line
-    var allVerbs = verbs.split('\n');
-    if (!allVerbs.includes(subjectLine.split(' ')[0].toLowerCase()))
-        return (false);
-    return (true);
-}
-
-function checkRule6() { // Wrap the body at 72 characters
-    return (true);
-}
-
-function checkRule7() { // Use the body to explain what and why vs. how
-    return (true);
 }
--- php/request.php
+++ php/request.php
@@ -7,5 +7,69 @@
         ]
     ];
     $context = stream_context_create($options);
-    echo(file_get_contents('https://api.github.com/repos/' . $_GET['author'] . '/' . $_GET['repo'] . '/commits?access_token=' . $token . '&per_page=100&page=' . $_GET['page'], false, $context));
+
+    $verbs = explode(PHP_EOL, file_get_contents("verbs.txt"));
+
+    $page = 1;
+    do
+    {
+        $commits = json_decode(file_get_contents('https://api.github.com/repos/' . $_GET['author'] . '/' . $_GET['repo'] . '/commits?access_token=' . $token . '&per_page=100&page=' . $page, false, $context));
+
+        $array = array();
+        $rules = array('checkRule1', 'checkRule2', 'checkRule3', 'checkRule4', 'checkRule5', 'checkRule6', 'checkRule7');
+        foreach ($commits as $i) {
+            $elem = json_decode(json_encode($i), true);
+            $rule = array();
+            $commitMsg = $elem['commit']['message'];
+            $subjectLine = explode(PHP_EOL, $commitMsg)[0];
+            if (!preg_match("/Merge branch[ 'a-zA-Z0-9_-]+ of https:\/\/github.com\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+/", $subjectLine)) {
+                foreach ($rules as $r) {
+                    array_push($rule, call_user_func($r, $subjectLine, $verbs));
+                }
+            }
+            else
+                array_push($rule, true, true, true, true, true, true, true);
+            array_push($array, array($elem['commit']['author']['name'], $commitMsg, $rule));
+        }
+        $page += 1;
+    } while (count($commits) == 100);
+
+    echo(json_encode($array));
+
+    function checkRule1() { // Separate subject from body with a blank line
+        return (true);
+    }
+
+    function checkRule2($subjectLine) { // Limit the subject line to 50 characters
+        if (strlen($subjectLine) > 50)
+            return (false);
+        return (true);
+    }
+
+    function checkRule3($subjectLine) { // Capitalize the subject line
+        if ($subjectLine[0] < 'A' || $subjectLine > 'Z')
+            return (false);
+        return (true);
+    }
+
+    function checkRule4($subjectLine) { // Do not end the subject line with a period
+        $last = substr($subjectLine, -1);
+        if ($last == '.' || $last == ',' || $last == ';' || $last == ':')
+            return (false);
+        return (true);
+    }
+
+    function checkRule5($subjectLine, $verbs) { // Use the imperative mood in the subject line
+        if (in_array(strtolower(explode(' ', $subjectLine)[0]), $verbs))
+            return (true);
+        return (false);
+    }
+
+    function checkRule6() { // Wrap the body at 72 characters
+        return (true);
+    }
+    
+    function checkRule7() { // Use the body to explain what and why vs. how
+        return (true);
+    }
 ?>
